6001d29c862dd2815bd88cdd5863debff2b2e84f
Verouge <branola1998628@gmail.com>
2023-08-10 20:18:47 +1000

    Fixed routing issues

--- server.js
+++ server.js
@@ -7,11 +7,12 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static("./public"));
 
 // Importing routes
-require("./routes/htmlRoutes")(app);
 require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 // Starting server
 app.listen(PORT, () => {
